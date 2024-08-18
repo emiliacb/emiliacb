151423a4b08fafd0484df8189d89de8b72521cb5
em3c2 <emiliacabralb@gmail.com>
2024-08-18 05:46:31 -0300

    removed dist

--- landing/src/index.ts
+++ landing/src/index.ts
@@ -2,10 +2,12 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import router from "./router";
 
-const app = new Hono()
-  .route("/", router)
+const app = new Hono().route("/", router);
+const port = parseInt(process.env.PORT ?? "", 10) || 3000;
+
+console.log(`\x1b[32mServer listening at port: \x1b[34m${port}\x1b[0m`);
 
 serve({
   fetch: app.fetch,
-  port: 3000,
+  port,
 });
