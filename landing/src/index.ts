62f36133f9d39ca5b7e32785c1f4755dc1cd0d5b
Emilia <66620157+emiliacb@users.noreply.github.com>
2024-08-26 17:23:28 -0300

    added cache

--- landing/src/index.ts
+++ landing/src/index.ts
@@ -1,19 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import router from "./router";
-import { cache } from 'hono/cache'
-
 
 const app = new Hono()
 const port = parseInt(process.env.PORT ?? "", 10) || 3000;
 
-app.get(
-  '*',
-  cache({
-    cacheName: 'my-app',
-    cacheControl: 'max-age=3600',
-  })
-)
 app.route("/", router);
 
 console.log(`\x1b[32mServer listening at port: \x1b[34m${port}\x1b[0m`);
--- landing/src/router.ts
+++ landing/src/router.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cache } from 'hono/cache'
 
 import homeHandler from "./routes/home";
 import blogHandler from "./routes/blog";
@@ -7,8 +8,15 @@ import aboutHandler from "./routes/about";
 import notFoundHandler from "./routes/not-found";
 
 const router = new Hono();
+const cacheVersion = 1;
 
 router
+  .get(
+    '*',
+    cache({
+      cacheName: `emiliacb-v${cacheVersion}`,
+      cacheControl: 'max-age=3600',
+    }))
   .get("/", homeHandler)
   .get("/about", aboutHandler)
   .get("/blog/*", blogHandler)
