106cb21c9309decc437ca4b9273733f6abfe285b
Emilia <66620157+emiliacb@users.noreply.github.com>
2024-08-26 16:43:38 -0300

    removed blog link

--- landing/content/pages/home.md
+++ landing/content/pages/home.md
@@ -3,6 +3,7 @@
 # Engineer, Designer or Librarian 
 
 With a unique blend of engineering, design, and AI expertise, I transform ideas into powerful digital solutions. Whether you're looking to build a scalable web application, integrate cutting-edge AI technologies, or enhance your team's skills through mentorship and training, I'm here to help you achieve your goals.
+[More about me](/about)
 
 📧 [emiliacabralb@gmail.com](mailto:emiliacabralb@gmail.com)  
 🔗 [LinkedIn](https://www.linkedin.com/in/emiliacb)  
--- landing/src/components/navbar/index.ts
+++ landing/src/components/navbar/index.ts
@@ -4,18 +4,13 @@ export default function navbar() {
   return html`
     <a class="sr-only focus:not-sr-only focus:absolute top-1 left-1 focus:p-3 text-white bg-black" href="#content">skip to content</a>
     <nav
-      class="sticky top-0 pt-2 md:pt-4 pb-2 mb-4 md:mb-0 flex mt-12 justify-between w-full max-w-[60rem] m-auto bg-stone-200/70 backdrop-blur-sm dark:bg-stone-800/80 border-b border-stone-400"
+      class="sticky top-0 pt-2 md:pt-4 pb-2 mb-4 md:mb-0 flex mt-12 justify-between w-full max-w-[60rem] m-auto bg-stone-200/70 backdrop-blur-sm dark:bg-stone-800/80 border-b border-stone-400 z-50"
     >
       <a class="px-2 py-0 h-fit font-[500] text-lg" href="/">emiliacb</a>
       <div class="flex space-x-4">
         <a class="px-2 py-0 h-fit" href="/about">about</a>
-        <a class="px-2 py-0 h-fit" href="/blog">blog</a>
         <a class="px-2 py-0 h-fit" href="/services">services</a>
       </div>
     </nav>
-    <!-- <div class="flex justify-end py-2 md:hidden ml-auto sticky top-[2.8rem] w-full bg-stone-200/70 backdrop-blur-sm dark:bg-stone-800/80">
-      <a class="px-2 py-0 h-fit" href="/blog">blog</a>
-      <a class="px-2 py-0 h-fit" href="/services">services</a>
-    </div> -->
   `;
 }
