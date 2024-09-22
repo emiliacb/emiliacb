07e1e109ab6fd658bb4f6b92eba017c027367140
em3c2 <emiliacabralb@gmail.com>
2024-09-21 23:46:28 -0300

    add cache

--- landing/src/index.ts
+++ landing/src/index.ts
@@ -1,12 +1,21 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cache } from 'hono/cache'
+
 import router from "./router";
 
 const app = new Hono()
-const port = parseInt(process.env.PORT ?? "", 10) || 3000;
+app.get(
+  '*',
+  cache({
+    cacheName: 'emiliacb',
+    cacheControl: 'public, max-age=86400',
+  })
+)
 
 app.route("/", router);
 
+const port = parseInt(process.env.PORT ?? "", 10) || 3000;
 console.log(`\x1b[32mServer listening at port: \x1b[34m${port}\x1b[0m`);
 
 serve({
--- landing/src/services/content.ts
+++ landing/src/services/content.ts
@@ -1,11 +1,22 @@
 import fs from "fs";
-import path from "path";
 import { marked } from "marked";
+
+const cache = new Map();
 
 export async function getContent(filePath: string) {
   try {
+    if (cache.has(filePath)) {
+      return cache.get(filePath);
+    }
+    if (!fs.existsSync(filePath)) {
+      throw new Error(`File not found: ${filePath}`);
+    }
+    if (!filePath.endsWith(".md")) {
+      throw new Error(`File must be a Markdown file: ${filePath}`);
+    }
     const file = fs.readFileSync(filePath, "utf8");
     const htmlContent = await marked(file);
+    cache.set(filePath, htmlContent);
 
     return htmlContent;
   } catch (error: any) {
