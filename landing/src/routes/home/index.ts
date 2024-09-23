53306abc244dab1b6b8e2ecb3a58e913babb6ac5
em3c2 <emiliacabralb@gmail.com>
2024-09-23 17:54:14 -0300

    error update

--- landing/src/routes/home/index.ts
+++ landing/src/routes/home/index.ts
@@ -10,33 +10,53 @@ export default async function handler(c: Context) {
   const htmlContent = await getContent(filePath);
 
   return c.html(html`
-     <div style="width: 100%; height: 100%; max-width: 40rem; display: grid; place-content: center; margin: auto;">
+    <div
+      style="width: 100%; height: 100%; max-width: 40rem; display: grid; place-content: center; margin: auto;"
+    >
       <p>Hello there! 👷‍♂️</p>
-      <p>Thank you for visiting our page. Unfortunately, we're experiencing some technical difficulties and our site is temporarily unavailable.</p>
+      <p>
+        Thank you for visiting our page. Unfortunately, the server is
+        experiencing some technical difficulties and, as this site is
+        temporarily unavailable.
+      </p>
+      <p>
+        I will fix this issue when I have some time.
+      </p>
     </div>
-  `)
+  `);
 
   const view = layout({
     siteData: {
       title: "ємιℓιαċв",
     },
-    children: html`
-    <div class="prose prose-stone dark:prose-invert text-pretty">
-      <div aria-hidden="true" class="font-mono text-[14px] origin-top-left scale-75 -mb-4 md:scale-90 md:mb-4  whitespace-pre leading-[6px]">
-        <div>           __________                              </div>
-        <div>         .'----------\`.                           </div>
-        <div>         | .--------. |                           </div>
-        <div>         | |<span class="text-white animate-pulse">########</span>| |         ________           </div>
-        <div>         | |<span class="text-white animate-pulse">########</span>| |       /_________\\          </div>
-        <div>.--------| \`--------' |------|    --=-- |--------. </div>
-        <div>|        \`----,-.-----'      |o ======  |        | </div>
-        <div>|       ______|_|_______     |__________|        | </div>
-        <div>|      /  %%%%%%%%%%%%  \\                        | </div>
-        <div>|     /  %%%%%%%%%%%%%%  \\                       | </div>
-        <div>|     ^^^^^^^^^^^^^^^^^^^^                       | </div>
-        <div>+-----------------------------------------------+  </div>
+    children: html` <div
+      class="prose prose-stone dark:prose-invert text-pretty"
+    >
+      <div
+        aria-hidden="true"
+        class="font-mono text-[14px] origin-top-left scale-75 -mb-4 md:scale-90 md:mb-4  whitespace-pre leading-[6px]"
+      >
+        <div>__________</div>
+        <div>.'----------\`.</div>
+        <div>| .--------. |</div>
+        <div>
+          | |<span class="text-white animate-pulse">########</span>| | ________
+        </div>
+        <div>
+          | |<span class="text-white animate-pulse">########</span>| |
+          /_________\\
+        </div>
+        <div>.--------| \`--------' |------| --=-- |--------.</div>
+        <div>| \`----,-.-----' |o ====== | |</div>
+        <div>| ______|_|_______ |__________| |</div>
+        <div>| / %%%%%%%%%%%% \\ |</div>
+        <div>| / %%%%%%%%%%%%%% \\ |</div>
+        <div>| ^^^^^^^^^^^^^^^^^^^^ |</div>
+        <div>+-----------------------------------------------+</div>
         <div>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^</div>
-        <div aria-hidden="true" class="opacity-0">from https://www.asciiart.eu/</div>
+        <div aria-hidden="true" class="opacity-0">
+          from https://www.asciiart.eu/
+        </div>
       </div>
       ${raw(htmlContent)}
     </div>`,
@@ -46,6 +66,6 @@ export default async function handler(c: Context) {
     headers: {
       "Content-Type": "text/html",
       "Cache-Control": "public, max-age=3605",
-    }
+    },
   });
 }
