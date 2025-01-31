9267c7f718fc9cc78640d2da49d34f3cf132c261
em3c2 <emiliacabralb@gmail.com>
2025-01-31 19:38:43 -0300

    Update SEO metadata to use dynamic site title and description

--- landing/src/components/layout/index.ts
+++ landing/src/components/layout/index.ts
@@ -31,21 +31,23 @@ export default function layout({
         
         <!-- Primary Meta Tags -->
         <title>${siteData.title}</title>
-        <meta name="title" content="Emilia" />
-        <meta name="description" content=${siteData.description} />
+        <meta name="title" content="${siteData.title}" />
+        <meta name="description" content="${siteData.description}" />
 
         <!-- Open Graph / Facebook -->
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://emiliacabral.com/" />
-        <meta property="og:title" content="Emilia" />
-        <meta property="og:description" content=${siteData.description} />
+        <meta property="og:title" content="${siteData.title}" />
+        <meta property="og:description" content="${siteData.description}" />
         <meta property="og:image" content="/public/preview.png" />
 
         <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:card" content="/public/preview.png" />
         <meta property="twitter:url" content="https://emiliacabral.com/" />
-        <meta property="twitter:title" content="Emilia" />
-        <meta property="twitter:description" content=${siteData.description} />
+        <meta property="twitter:title" content="${siteData.title}" />
+        <meta property="twitter:description" content="${
+          siteData.description
+        }" />
         <meta property="twitter:image" content="/public/preview.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
