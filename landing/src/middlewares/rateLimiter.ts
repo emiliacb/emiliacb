0bdb6a85c3bbf93a7b379bd195b5922c90b4b166
Emilia <66620157+emiliacb@users.noreply.github.com>
2025-10-15 00:06:15 +0200

    Update rate limiter configuration to enhance request handling
    - Increased points to 120 and reduced duration to 60 seconds for improved rate limiting efficiency.

--- landing/src/middlewares/rateLimiter.ts
+++ landing/src/middlewares/rateLimiter.ts
@@ -2,8 +2,8 @@ import { RateLimiterMemory } from "rate-limiter-flexible";
 
 const BLOCK_SECONDS: number = 120;
 const ipLimiter = new RateLimiterMemory({
-  points: 60,
-  duration: 120,
+  points: 120,
+  duration: 60,
 });
 
 export const rateLimiterMiddleware = async (c: any, next: any) => {
